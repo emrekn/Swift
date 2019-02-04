4e7da7121008d12e76fcd63ce727ab69f18f6b44
emrekn <46498401+emrekn@users.noreply.github.com>
2019-02-04 22:46:39 +0300

    Add files via upload

---
+++ Emre_Playground.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+/*
+ Swift Öğrenme
+ */
+var str = "Hello, playground" //String tip bir değişken tanımlama
+
+print(str) // yazı yazdır
+
+var x = 1 // değişken tanımlama
+var y = 2 // değişken tanımlama
+
+x + y
+
+// değişkenler
+//var = değeri değişecek değişkenler için kullanılır
+//var sayi = 20
+
+//let = sabit değişken tanımlamada kullanılır
+//let sabitDegisken = 20
+
+
+var k = "emre"
+print(k)
+print("emanet")
+
+//String
+var s = "Yeni bir programlama dili olan"
+print(s + " Swift öğreniyorum")
+
+//Integer
+var sayi = 1
+var sayac:Int = 3
+
+//Double, Decimal, Float
+var piSayisi = 3.14 // double tanımlama
+var dolarKuru:Double = 3.73
+print(piSayisi)
+
+//Type Casting
